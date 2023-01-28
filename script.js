9f4a2d4fbace54ad289924bf4115c60cd47e3b9f
Isaac <52172215+noproxy88@users.noreply.github.com>
2023-01-28 20:02:05 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//we need to create variable pertaining to the ID
+
+//the input of user
+let input = document.querySelector('#cityInput')
+let inputBtn = document.querySelector('#citySearch')
+
+//the output of user input
+let cityOutput = document.querySelector('#City')
+let desc = document.querySeelector('#Desc')
+let temp = document.querySelector('#Temp')
+let wind = document.querySelector('#Wind')
